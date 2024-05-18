2f12672d13a1fe7fa6d7332b884fa501033b6f83
Jordan Van Aalsburg <jordan.vanaalsburg@usace.army.mil>
2024-05-18 12:55:27 -0700

    Enable Go LSP and formatter

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -20,6 +20,11 @@ return {
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 			local lspconfig = require("lspconfig")
+
+			lspconfig.gopls.setup({
+				capabilities = capabilities,
+			})
+
 			lspconfig.lua_ls.setup({
 				capabilities = capabilities,
 			})
