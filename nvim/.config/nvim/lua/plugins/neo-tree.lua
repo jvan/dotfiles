df95617fbf660b63151feece5378d21dd6868142
Jordan Van Aalsburg <jordan.vanaalsburg@usace.army.mil>
2025-02-07 14:45:15 -0800

    Add toggle for Neotree panel

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -8,6 +8,7 @@ return {
 	},
 	config = function()
 		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
+		vim.keymap.set("n", "<leader>tf", ":Neotree filesystem toggle left<CR>", {})
 		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
 	end,
 }
