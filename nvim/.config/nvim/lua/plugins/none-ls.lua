2330977dbc6d5461f2302ca915b5daa0b9a98d1e
Jordan Van Aalsburg <jordan.vanaalsburg@usace.army.mil>
2025-02-07 14:46:26 -0800

    Add formatting for JavaScript and SQL files

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -8,6 +8,8 @@ return {
 				null_ls.builtins.formatting.black,
 				null_ls.builtins.formatting.isort,
 				null_ls.builtins.formatting.gofumpt,
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.formatting.sql_formatter.with({ command = { "sleek" } }),
 			},
 		})
 
