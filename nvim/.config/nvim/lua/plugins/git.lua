3dd2436c935c92a87ec3b7d663c971019ff5ed83
Jordan Van Aalsburg <jordan.vanaalsburg@gmail.com>
2025-05-01 09:44:04 -0700

    Replace vim-fugitive with neogit

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -1,6 +1,15 @@
 return {
 	{
-		"tpope/vim-fugitive",
+		"NeogitOrg/neogit",
+		version = "0.0.1",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"sindrets/diffview.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+		config = function()
+			require("neogit").setup({})
+		end,
 	},
 	{
 		"lewis6991/gitsigns.nvim",
